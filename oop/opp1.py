3038a34aec0b9ee4e6c8e28b80633bed385c8872
quadrant26 <quadrant26@163.com>
2016-10-10 18:09:01 +0800

    oop

---
+++ oop/opp1.py
@@ -0,0 +1,12 @@
+print("fishc.com".count('o'))
+print([1,1,2,3,5,8].count(1))
+print((0,2,4,8,12,18).count(1))
+
+class MyClass:
+    name = "FishC"
+    
+    def myFun(self):
+        print("hello FishC!")
+    
+print(MyClass.name)
+MyClass.myFun()
