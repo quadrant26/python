7c9bb1d1262476c80b64711237cc7ebef827e162
quadrant26 <quadrant26@163.com>
2016-10-13 11:07:50 +0800

    工厂函数

--- magic/m_2.py
+++ magic/m_2.py
@@ -8,7 +8,7 @@ class New_int(int):
     def __add__(self, other):
         return int.__sub__(self, other)
     def __sub__(self, other):
-        return int.__add(self, other)
+        return int.__add__(self, other)
 
 a = New_int(3)
 b = New_int(5)
