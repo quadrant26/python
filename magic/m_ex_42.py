179c8670f4df6afb2ce6aa85b5183b5809107f48
quadrant26 <quadrant26@163.com>
2016-10-13 18:52:40 +0800

    统计函数的调用的时间 默认是100000 新增一个timing() 用来统计时间

--- magic/m_ex_42.py
+++ magic/m_ex_42.py
@@ -1,15 +1,15 @@
 # 计算时间差值
 import time as t
-# perf_counter() 返回计时器的精准时间
-# process_time() 返回当前进程执行 CPU 的时间总和
         
 class Time:
-    def __init__(self, name="perf_counter"):
+    def __init__(self, func, number=100000):
         # self.unit = ["年", "月", "日", "时", "分", "秒"]
         self.begin = 0
         self.end = 0
         self.prompt = "未开始计时"
-        self.name = name
+        self.default_time = t.perf_counter
+        self.func = func
+        self.number = number
             
     def __str__(self):
         return self.prompt
@@ -17,56 +17,25 @@ class Time:
     __repr__ = __str__
     
     def __add__(self, other):
-        self.prompt = "一共花了"
-        result = []
-        '''
-        for index in range(6):
-            result.append(self.lasted[index] + other.lasted[index])
-            if not result[index]:
-                self.prompt += (str(result[index]) + self.unit[index])
-        '''
-        result.append(self.lasted[0] + other.lasted[0])
-        self.prompt += (result[0] + '秒')
-        return self.prompt
+        result = self.lasted + other.lasted
+        prompt = "总共花了 %.2f 秒" % result
+        return prompt
     
+    # 设置定时器为 t.perf_counter or t.process_time
     def set_time(self, name):
-        self.name = name
-    
-    def start(self):
-        if self.name == "perf_counter":
-            self.begin = t.perf_counter()
-        elif self.name == "process_time":
-            self.begin = t.process_time()
-        # 没有  调用方法时 stop()
-        self.prompt = "提示：请先调用stop() 停止计时"
-        print("开始计时")
-        
-    def stop(self):
-        if self.begin:
-            if self.name == "perf_counter":
-                self.end = t.perf_counter()
-            elif self.name == "process_time":
-                self.end = t.process_time()
-                
-            self._calc();
-            print("计时结束")
+        if name == "perf_counter":
+            self.default_time = t.perf_counter
+        elif name == "process_time":
+            self.default_time = t.process_time
         else:
-            print("提示： 请开始 start() 开始计时")
+            print("您的输入有误！")
             
-    def _calc(self):
-        self.prompt = "总共花了"
-        self.lasted = []
-        '''
-        for index in range(6):
-            self.lasted.append(self.end[index] - self.begin(index))
-            if not self.lasted[index]:
-                self.prompt += (str(self.lasted[index]) + self.unit[index])
-        '''
-        self.lasted.append(self.end - self.begin)
-        self.prompt += str(self.lasted[0]) + '秒'
-
-        # 重新初始化
-        self.begin = 0
-        self.end = 0
+    def timing(self):
+        self.begin = self.default_time()
+        for i in range(self.number):
+            self.func()
+        self.end = self.default_time()
+        self.lasted = self.end - self.begin
+        self.prompt = "总共花了 %.2f 秒" % self.lasted
                 
     
