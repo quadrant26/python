02591ac38400125f1ec637e92e83e9b785edece3
quadrant26 <quadrant26@163.com>
2016-10-13 16:31:10 +0800

    相减操作，去除 a 在 b 中的子字符串

--- magic/m_ex_2.py
+++ magic/m_ex_2.py
@@ -24,13 +24,12 @@ class Nstr2(str):
 	def __rshift__(self, other):
 		return self[:-other] + self[-other:]
 
-
 a = Nstr2("You can you up, No can no BB")
 print(a << 4)
 print(a >> 4)
 
 def Nstr3(str):
-        '''
+    '''
 	def __init__(self, str=""):
 		self.result = 0
 		for each in str:
@@ -47,31 +46,32 @@ def Nstr3(str):
 
 	def __truediv__(self, other):
 		return str.__truediv__(self, ord(other))
-        '''
-        def __init__(self, arg=''):
-                if isinstance(arg, str):
-                        self.total = 0
-                        for each in arg:
-                                    self.total += ord(each)
-                else:
-                        print("参数错误")
+    '''
+   
+    def __init__(self, arg=''):
+        if isinstance(arg, str):
+            self.total = 0
+            for each in arg:
+                self.total += ord(each)
+        else:
+            print("参数错误")
 
-        def __add__(self, other):
-                return self.total + other.total
-        def __sub__(self, other):
-                return self.total - other.total
-        def __mul__(self, other):
-                return self.total * other.total
-        def __truediv__(self, other):
-                return self.total / other.total
-        def __floordiv__(self, other):
-                return self.total // other.total
+    def __add__(self, other):
+        return self.total + other.total
+    def __sub__(self, other):
+        return self.total - other.total
+    def __mul__(self, other):
+        return self.total * other.total
+    def __truediv__(self, other):
+        return self.total / other.total
+    def __floordiv__(self, other):
+        return self.total // other.total
 
-def Nstr4(str):
-        def __new__(cls, other):
-                if isinstance(arg, str):
-                        total = 0
-                        for each in arg:
-                                total += ord(each)
-                        arg = total
-                return int.__new__(cls, arg)
+class Nstr4(str):
+    def __new__(cls, other):
+        if isinstance(other, str):
+            total = 0
+            for each in other:
+                 total += ord(each)
+            arg = total
+        return int.__new__(cls, arg)
