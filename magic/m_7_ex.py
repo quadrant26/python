e0972d92f264212aa7a15f869662845da9e1b162
quadrant26 <quadrant26@163.com>
2016-10-13 11:01:59 +0800

    定制list 支持 append() pop() extend()

---
+++ magic/m_7_ex.py
@@ -0,0 +1,57 @@
+# 定制list 支持 append() pop() extend()
+'''
+1. 实现获取、设置和删除一个元素的行为（ 计数器也会被删除 ）
+2. counter(index) 返回 index 参数所指定的元素记录的访问次数
+3. 实现 append() pop() remove() insert() clear() reverse() 方法
+'''
+class CountList(list):
+        def __init__(self, *args):
+                super().__init__(args)
+                self.count = []
+                for i in args:
+                        self.count.append(0)
+
+        def __len__(self):
+                return len(self.count)
+
+        def __getitem__(self, key):
+                self.count[key] += 1
+                return super().__getitem__(key)
+
+        def __setitem__(self, key, value):
+                self.count[key] += 1
+                super().__setitem__(key, value)
+
+        def __delitem__(self, key):
+                del self.count[key]
+                super().__delitem__(key)
+
+        def counter(self, key):
+                return self.count[key]
+
+        def append(self, value):
+                self.count.append(0)
+                super().append(value)
+
+        def pop(self, key=-1):
+                def self.count[key]
+                return super().pop(key)
+
+        def remove(self, value):
+                key = super().index(value)
+                del self.count[key]
+                super().insert(key, value)
+
+        def insert(self, key, value):
+                self.count.insert(key, 0)
+                super().insert(key, value)
+
+        def clear(self):
+                self.count.clear()
+                super().clear()
+
+
+        def reverse(self):
+                self.count.reverse()
+                super().reverse()
+                
