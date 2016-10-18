0a7ee9ca70766359373ec63e9cd02334e98d60df
quadrant26 <quadrant26@163.com>
2016-10-18 18:44:37 +0800

    Scrapy pip install lxml pyOpenSSL

---
+++ Crawler/scrapy.py
@@ -0,0 +1,18 @@
+'''
+Scrapy
+
+https://sourceforge.net/projects/pywin32
+
+安装 pip 
+    1. get_pip.py
+    2. python get_pip.py
+    3. 出现问题
+        a: python27/lib/site-packages -> new file sitecoumtomize.py
+        b: ipmort sys
+        c: sys.setdefaultcoding("gb2312")
+
+pip install lxml
+pip install pyOpenSSL
+pip install Scrapy
+
+'''
