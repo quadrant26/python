221f1885229a0294c6a880433d58dced403766e9
quadrant26 <quadrant26@163.com>
2016-10-21 18:20:49 +0800

    Menubutton() Menu().add_command(...)

---
+++ Tkinter/test_menu4_Menubutton.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+def callback():
+    print("你好...")
+
+mb = Menubutton(root, text="点我", relief=RAISED)
+mb.pack()
+
+filemenu = Menu(mb, tearoff=False)
+filemenu.add_command(label="打开", command=callback)
+filemenu.add_command(label="保存", command=callback)
+filemenu.add_separator() # 分割线
+filemenu.add_command(label="退出", command=root.quit)
+
+
+mb.config(menu=filemenu)
+
+
+root.mainloop()
