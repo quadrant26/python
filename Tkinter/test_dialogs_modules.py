32b48e6d50aeb99e101a50c046dc276a0a21553c
quadrant26 <quadrant26@163.com>
2016-10-23 14:41:32 +0800

    Tkinter messagebox.askokcancel(title, message, options)

---
+++ Tkinter/test_dialogs_modules.py
@@ -0,0 +1,21 @@
+'''
+标准对话框
+ messagebox
+ filedialog
+ colorchooser
+
+ arguments：
+ title       标题
+ message     消息内容
+ options     参数
+     default:
+     icon       图标
+     parent     
+'''
+
+from tkinter import *
+
+print(messagebox.askokcancel("python","发射"))
+
+mainloop()
+
